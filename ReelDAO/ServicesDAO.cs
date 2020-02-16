f7cd154963322c8e56dd37a0915480afa96ad4a9
SSiddagowni.Dicetek <SSiddagowni.Dicetek@EMRITDT0454>
2020-02-16 16:37:12 +0400

    Added new api's

--- ReelDAO/ServicesDAO.cs
+++ ReelDAO/ServicesDAO.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Practices.EnterpriseLibrary.Data;
+﻿using EEG_ReelCinemasRESTAPI.Models;
+using Microsoft.Practices.EnterpriseLibrary.Data;
 using Newtonsoft.Json;
 using ReelDAO;
 using ReelDvo;
@@ -26,6 +27,93 @@ namespace ReelDAO
             {
                 db = DatabaseFactory.CreateDatabase("ServicesConString");
                 DbCommand command = db.GetStoredProcCommand("GetAllMasterServices");
+
+                return db.ExecuteDataSet(command).Tables[0];
+            }
+            catch (Exception ex)
+            {
+                //new LogDao().InsertLog("", "ERROR", "MobileBookingDao", "GetServiceSettings", "", "REQUEST", JsonConvert.SerializeObject(ex), "MOBILE", null);
+                return null;
+            }
+            finally
+            {
+
+            }
+        }
+        public DataTable GetUserDetailsByEmailId(string EmailId)
+        {
+            try
+            {
+                db = DatabaseFactory.CreateDatabase("ServicesConString");
+                DbCommand command = db.GetStoredProcCommand("GetUserDetailsByEmailId");
+                db.AddInParameter(command, "EmailId", DbType.String, EmailId);
+
+                return db.ExecuteDataSet(command).Tables[0];
+            }
+            catch (Exception ex)
+            {
+                //new LogDao().InsertLog("", "ERROR", "MobileBookingDao", "GetServiceSettings", "", "REQUEST", JsonConvert.SerializeObject(ex), "MOBILE", null);
+                return null;
+            }
+            finally
+            {
+
+            }
+        }
+        public DataTable GetAllCompanyServices(Int64 MasterServiceID,Int64 CompanyID)
+        {
+            try
+            {
+                db = DatabaseFactory.CreateDatabase("ServicesConString");
+                DbCommand command = db.GetStoredProcCommand("GetAllCompanyServices");
+                db.AddInParameter(command, "MasterServiceID", DbType.Int64, MasterServiceID);
+                db.AddInParameter(command, "CompanyID", DbType.Int64, CompanyID);
+
+
+                return db.ExecuteDataSet(command).Tables[0];
+            }
+            catch (Exception ex)
+            {
+                //new LogDao().InsertLog("", "ERROR", "MobileBookingDao", "GetServiceSettings", "", "REQUEST", JsonConvert.SerializeObject(ex), "MOBILE", null);
+                return null;
+            }
+            finally
+            {
+
+            }
+        }
+
+        public DataTable GetAllServicesRequests(GetAllServicesRequestsReq req)
+        {
+            try
+            {
+                db = DatabaseFactory.CreateDatabase("ServicesConString");
+                DbCommand command = db.GetStoredProcCommand("GetAllServicesRequests");
+                db.AddInParameter(command, "MasterServiceID", DbType.Int64, req.MasterServiceId);
+                db.AddInParameter(command, "CompanyID", DbType.Int64, req.CompanyId);
+                db.AddInParameter(command, "CompanyServiceId", DbType.Int64, req.CompanyServiceId);
+
+
+
+                return db.ExecuteDataSet(command).Tables[0];
+            }
+            catch (Exception ex)
+            {
+                //new LogDao().InsertLog("", "ERROR", "MobileBookingDao", "GetServiceSettings", "", "REQUEST", JsonConvert.SerializeObject(ex), "MOBILE", null);
+                return null;
+            }
+            finally
+            {
+
+            }
+        }
+
+        public DataTable GetAllCompanies()
+        {
+            try
+            {
+                db = DatabaseFactory.CreateDatabase("ServicesConString");
+                DbCommand command = db.GetStoredProcCommand("GetAllCompanies");
                 
                 return db.ExecuteDataSet(command).Tables[0];
             }
@@ -39,27 +127,234 @@ namespace ReelDAO
 
             }
         }
-        public DataTable GetAllCompanyServices(Int64 MasterServiceID)
+
+        public Int64 CreateCompany(string CompanyName, string Description)
         {
+            Int64 CompanyID = 0;
+            int result = 0;
             try
             {
-                db = DatabaseFactory.CreateDatabase("ServicesConString");
-                DbCommand command = db.GetStoredProcCommand("GetAllCompanyServices");
-                db.AddInParameter(command, "MasterServiceID", DbType.Int64, MasterServiceID);
+                log = new LogDao();
 
-                return db.ExecuteDataSet(command).Tables[0];
+                DbCommand command = null;
+                db = DatabaseFactory.CreateDatabase("ServicesConString");
+                command = db.GetStoredProcCommand("CreateCompany");
+                db.AddInParameter(command, "CompanyName", DbType.String, CompanyName);
+                db.AddInParameter(command, "Description", DbType.String, Description);
+                db.AddOutParameter(command, "CompanyID", DbType.Int64, 10);
+                result = db.ExecuteNonQuery(command);
+                CompanyID = int.Parse(db.GetParameterValue(command, "CompanyID").ToString());
             }
             catch (Exception ex)
             {
-                //new LogDao().InsertLog("", "ERROR", "MobileBookingDao", "GetServiceSettings", "", "REQUEST", JsonConvert.SerializeObject(ex), "MOBILE", null);
-                return null;
+
             }
             finally
             {
 
             }
+            return CompanyID;
         }
 
+
+        public void ServiceRequest(ServiceRequestReq req)
+        {
+
+            int result = 0;
+            try
+            {
+                log = new LogDao();
+
+                DbCommand command = null;
+                db = DatabaseFactory.CreateDatabase("ServicesConString");
+                command = db.GetStoredProcCommand("ServiceRequest");
+                db.AddInParameter(command, "CompanyServiceID", DbType.Int64, req.CompanyServiceID);
+                db.AddInParameter(command, "FullName", DbType.String, req.FullName);
+                db.AddInParameter(command, "EmailID", DbType.String, req.EmailID);
+                db.AddInParameter(command, "MobileNoCC", DbType.String, req.MobileNoCC);
+                db.AddInParameter(command, "MobileNo", DbType.String, req.MobileNo);
+                db.AddInParameter(command, "Description", DbType.String, req.Description);
+                result = db.ExecuteNonQuery(command);
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+
+            }
+
+        }
+
+        public void AddServiceViewCount(Int64 ServiceID)
+        {
+
+            int result = 0;
+            try
+            {
+                log = new LogDao();
+
+                DbCommand command = null;
+                db = DatabaseFactory.CreateDatabase("ServicesConString");
+                command = db.GetStoredProcCommand("AddServiceViewCount");
+                db.AddInParameter(command, "ServiceID", DbType.Int64, ServiceID);
+                result = db.ExecuteNonQuery(command);
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+
+            }
+
+        }
+        public void AddUpdateServicesMedia(Int64 ServicesID, string Filenames,string Filepaths)
+        {
+            
+            int result = 0;
+            try
+            {
+                log = new LogDao();
+
+                DbCommand command = null;
+                db = DatabaseFactory.CreateDatabase("ServicesConString");
+                command = db.GetStoredProcCommand("AddUpdateServicesMedia");
+                db.AddInParameter(command, "ServicesID", DbType.Int64, ServicesID);
+                db.AddInParameter(command, "Filenames", DbType.String, Filenames);
+                db.AddInParameter(command, "Filepaths", DbType.String, Filepaths);
+
+                result = db.ExecuteNonQuery(command);
+                
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+
+            }
+            
+        }
+
+        public void AddUpdateProfileMedia(Int64 CompanyID, string Filenames, string Filepaths)
+        {
+
+            int result = 0;
+            try
+            {
+                log = new LogDao();
+
+                DbCommand command = null;
+                db = DatabaseFactory.CreateDatabase("ServicesConString");
+                command = db.GetStoredProcCommand("AddUpdateProfileMedia");
+                db.AddInParameter(command, "CompanyID", DbType.Int64, CompanyID);
+                db.AddInParameter(command, "Filenames", DbType.String, Filenames);
+                db.AddInParameter(command, "Filepaths", DbType.String, Filepaths);
+
+                result = db.ExecuteNonQuery(command);
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+
+            }
+
+        }
+
+        public Int64 CreateService(CreateServiceReq req, out bool status, out string statusMessage)
+        {
+            Int64 CompanyServiceID = 0;
+            int result = 0;
+            try
+            {
+                log = new LogDao();
+
+                DbCommand command = null;
+                db = DatabaseFactory.CreateDatabase("ServicesConString");
+                command = db.GetStoredProcCommand("CreateService");
+                db.AddInParameter(command, "CompanyID", DbType.Int64, req.CompanyID);
+                db.AddInParameter(command, "MasterServiceID", DbType.Int32, req.MasterServiceID);
+                db.AddInParameter(command, "CreatedBy", DbType.Int64, req.CreatedBy);
+                db.AddInParameter(command, "ServiceTitle", DbType.String, req.ServiceTitle);
+                db.AddInParameter(command, "ServiceDescription", DbType.String, req.ServiceDescription);
+                db.AddInParameter(command, "Timings", DbType.String, req.Timings);
+
+                db.AddOutParameter(command, "CompanyServiceID", DbType.Int64, 10);
+                db.AddOutParameter(command, "Status", DbType.Boolean, 10);
+                db.AddOutParameter(command, "StatusMessage", DbType.String, 50);
+                result = db.ExecuteNonQuery(command);
+                CompanyServiceID = int.Parse(db.GetParameterValue(command, "CompanyServiceID").ToString());
+                status = Convert.ToBoolean(db.GetParameterValue(command, "Status"));
+                statusMessage = db.GetParameterValue(command, "StatusMessage").ToString();
+            }
+            catch (Exception ex)
+            {
+                status = false;
+                statusMessage = ex.Message;
+            }
+            finally
+            {
+
+            }
+            return CompanyServiceID;
+        }
+
+        public Int64 CreateUser(string EmailId, string Password, string PasswordSalt, string FirstName, string LastName, string Gender, DateTime DOB, string MobileNoCountryCode,
+            string MobileNo, string PhoneNoCountryCode, string PhoneNo, Int64 CompanyID, out bool status, out string statusMessage)
+        {
+            Int64 UserID = 0;
+            int result = 0;
+            try
+            {
+                log = new LogDao();
+
+                DbCommand command = null;
+                db = DatabaseFactory.CreateDatabase("ServicesConString");
+                command = db.GetStoredProcCommand("CreateUser");
+
+                db.AddInParameter(command, "EmailId", DbType.String, EmailId);
+                db.AddInParameter(command, "Password", DbType.String, Password);
+                db.AddInParameter(command, "PasswordSalt", DbType.String, PasswordSalt);
+                db.AddInParameter(command, "FirstName", DbType.String, FirstName);
+                db.AddInParameter(command, "LastName", DbType.String, LastName);
+                db.AddInParameter(command, "Gender", DbType.String, Gender);
+                db.AddInParameter(command, "DOB", DbType.Date, DOB);
+                db.AddInParameter(command, "MobileNoCountryCode", DbType.String, MobileNoCountryCode);
+                db.AddInParameter(command, "MobileNo", DbType.String, MobileNo);
+                db.AddInParameter(command, "PhoneNoCountryCode", DbType.String, PhoneNoCountryCode);
+                db.AddInParameter(command, "PhoneNo", DbType.String, PhoneNo);
+                db.AddInParameter(command, "CompanyID", DbType.Int64, CompanyID);
+
+                db.AddOutParameter(command, "UserID", DbType.Int64, 10);
+                db.AddOutParameter(command, "Status", DbType.Boolean, 10);
+                db.AddOutParameter(command, "StatusMessage", DbType.String, 50);
+                result = db.ExecuteNonQuery(command);
+                UserID = Int64.Parse(db.GetParameterValue(command, "UserID").ToString());
+                status = Convert.ToBoolean(db.GetParameterValue(command, "Status"));
+                statusMessage = db.GetParameterValue(command, "StatusMessage").ToString();
+
+            }
+            catch (Exception ex)
+            {
+                status = false;
+                statusMessage = ex.Message;
+            }
+            finally
+            {
+
+            }
+            return UserID;
+        }
         public int UpdateNewsLetterFlag(string EmailId, bool isNewsletter, bool isIntrestedForLuckyDraw)
         {
 
---
+++ ReelDVO/ServicesBO.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EEG_ReelCinemasRESTAPI.Models
+{
+    public class ServicesBO
+    {
+    }
+
+
+    public class GetAllCompanyServicesReq
+    {
+        public Int64 MasterServiceID { get; set; }
+        public Int64 CompanyID { get; set; }
+    }
+
+    public class GetAllMasterServicesResp
+    {
+        public GetAllMasterServicesRespData[] data { get; set; }
+    }
+
+    public class GetAllMasterServicesRespData
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+        public string ImagePath { get; set; }
+    }
+    public class RegisterResp
+    {
+        public bool status { get; set; }
+        public string statusMessage { get; set; }
+    }
+    public class ValidateUserResp
+    {
+        public bool status { get; set; }
+        public string statusMessage { get; set; }
+        public UserBO UserObject { get; set; }
+    }
+
+    public class UserBO
+    {
+        public Int64 UserId { get; set; }
+        public string EmailId { get; set; }
+        public string Password { get; set; }
+        public string PasswordSalt { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public DateTime? DOB { get; set; }
+        public string MobileNoCountryCode { get; set; }
+        public string MobileNo { get; set; }
+        public string PhoneNoCountryCode { get; set; }
+        public string PhoneNo { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public bool IsActive { get; set; }
+        public Int64 CompanyID { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+
+    }
+
+    public class ValidateLoginReq
+    {
+        public string EmailId { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class RegisterReq
+    {
+        public string CompanyName { get; set; }
+        public string Description { get; set; }
+
+        public string EmailId { get; set; }
+        public string Password { get; set; }
+        public string PasswordSalt { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public DateTime DOB { get; set; }
+        public string MobileNoCountryCode { get; set; }
+        public string MobileNo { get; set; }
+        public string PhoneNoCountryCode { get; set; }
+        public string PhoneNo { get; set; }
+        public Int64 CompanyID { get; set; }
+
+        public string Filenames { get; set; }
+        public string Filepaths { get; set; }
+
+    }
+    public class CreateServiceReq
+    {
+        public Int64 CompanyID { get; set; }
+        public int MasterServiceID { get; set; }
+        public Int64 CreatedBy { get; set; }
+        public string ServiceTitle { get; set; }
+        public string ServiceDescription { get; set; }
+        public string Timings { get; set; }
+        public string Filenames { get; set; }
+        public string Filepaths { get; set; }
+    }
+
+    public class ServiceRequestReq
+    {
+        public Int64 CompanyServiceID { get; set; }
+        public string FullName { get; set; }
+        public string EmailID { get; set; }
+        public string MobileNoCC { get; set; }
+        public string MobileNo { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class AddServiceViewCountReq {
+        public Int64 ServiceID { get; set; }
+    }
+
+    public class GetAllServicesRequestsReq {
+
+        public Int64 CompanyId { get; set; }
+        public Int64 MasterServiceId { get; set; }
+        public Int64 CompanyServiceId { get; set; }
+
+    }
+
+}
